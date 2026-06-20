c2f138b3d167485193053c84ce749e08584e968b
Marton Leitold <lmarti1995@gmail.com>
2026-06-20 21:28:56 +0100

    Adding 2015 day2

--- 2015/Day2/Year2015Day2.cpp
+++ 2015/Day2/Year2015Day2.cpp
@@ -12,7 +12,18 @@ void Year2015Day2::Run(std::string filenamePath) {
 void Year2015Day2::RunPart1(std::string filenamePath) {
     std::cout << "Part 1" << std::endl;
     auto lines = ReadHelper::ReadLines(filenamePath);
-    int result = 0;
+
+    long result = 0;
+
+    for (const auto& line : lines) {
+        auto splitted = StringHelper::SplitString(line, "x");
+        long l = std::stoi(splitted[0]);
+        long w = std::stoi(splitted[1]);
+        long h = std::stoi(splitted[2]);
+
+        result += 2*l*w+2*l*h+2*w*h+std::min({l*w, l*h, w*h});
+    }
+
 
     std::cout << "Result: " << result << std::endl;
 }
@@ -20,7 +31,17 @@ void Year2015Day2::RunPart1(std::string filenamePath) {
 void Year2015Day2::RunPart2(std::string filenamePath) {
     std::cout << std::endl << "Part 2" << std::endl;
     auto lines = ReadHelper::ReadLines(filenamePath);
-    int result = 0;
+
+    long result = 0;
+
+    for (const auto& line : lines) {
+        auto splitted = StringHelper::SplitString(line, "x");
+        long l = std::stoi(splitted[0]);
+        long w = std::stoi(splitted[1]);
+        long h = std::stoi(splitted[2]);
+
+        result += l*w*h + 2*(l+w+h - std::max({l,w,h}));
+    }
 
     std::cout << "Result: " << result << std::endl;
 }
