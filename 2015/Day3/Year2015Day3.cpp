06ef61f47c6c61fefa112e11acbe0f3dea593c84
Marton Leitold <lmarti1995@gmail.com>
2026-06-21 09:30:33 +0200

    Adding 2015 day3

--- 2015/Day3/Year2015Day3.cpp
+++ 2015/Day3/Year2015Day3.cpp
@@ -3,6 +3,7 @@
 #include "../../Shared/StringHelper.h"
 
 #include <iostream>
+#include <set>
 
 void Year2015Day3::Run(std::string filenamePath) {
     RunPart1(filenamePath);
@@ -14,6 +15,30 @@ void Year2015Day3::RunPart1(std::string filenamePath) {
     auto lines = ReadHelper::ReadLines(filenamePath);
     int result = 0;
 
+    std::set<std::pair<int, int>> visitedHouses;
+
+    int x{0}, y{0};
+    visitedHouses.emplace(std::make_pair(x, y));
+
+    for (const auto& line : lines) {
+        for (char c : line) {
+            if (c == '^' ) {
+                x++;
+            }
+            else if (c == 'v') {
+                x--;
+            }
+            else if (c == '<') {
+                y--;
+            } else {
+                y++;
+            }
+            visitedHouses.emplace(std::make_pair(x, y));
+        }
+    }
+
+    result = visitedHouses.size();
+
     std::cout << "Result: " << result << std::endl;
 }
 
@@ -21,6 +46,56 @@ void Year2015Day3::RunPart2(std::string filenamePath) {
     std::cout << std::endl << "Part 2" << std::endl;
     auto lines = ReadHelper::ReadLines(filenamePath);
     int result = 0;
+    std::set<std::pair<int, int>> visitedHouses;
+
+    int santaX{0}, santaY{0}, robotX{0}, robotY{0};
+    visitedHouses.emplace(std::make_pair(santaX, santaY));
+
+    int i = 0;
+    for (const auto& line : lines) {
+        for (char c : line) {
+            if (c == '^' ) {
+                if (i % 2 == 0) {
+                    santaX++;
+                }
+                else {
+                    robotX++;
+                }
+            }
+            else if (c == 'v') {
+                if (i % 2 == 0) {
+                    santaX--;
+                }
+                else {
+                    robotX--;
+                }
+            }
+            else if (c == '<') {
+                if (i % 2 == 0) {
+                    santaY--;
+                }
+                else {
+                    robotY--;
+                }
+            } else {
+                if (i % 2 == 0) {
+                    santaY++;
+                }
+                else {
+                    robotY++;
+                }
+            }
+            if (i % 2 == 0) {
+                visitedHouses.emplace(std::make_pair(santaX, santaY));
+            }
+            else {
+                visitedHouses.emplace(std::make_pair(robotX, robotY));
+            }
+            i++;
+        }
+    }
+
+    result = visitedHouses.size();
 
     std::cout << "Result: " << result << std::endl;
 }
